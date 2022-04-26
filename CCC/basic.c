652b0b22568f19d50bbc75b6b961ac24faed0511
VikashPR <vikashrajendran2806@gmail.com>
2022-04-26 09:28:25 +0530

    Feat: leap year update

--- CCC/basic.c
+++ CCC/basic.c
@@ -5,27 +5,14 @@ int main()
     int n;
     scanf("%d", &n);
     // find if the given  year is a leap year or not
-    if (n % 4 == 0)
-    {
-        if (n % 100 == 0)
-        {
-            if (n % 400 == 0)
-            {
-                printf("%d is a leap year", n);
-            }
-            else
-            {
-                printf("%d is not a leap year", n);
-            }
-        }
-        else
-        {
-            printf("%d is a leap year", n);
-        }
+    if(n % 400 == 0){
+        printf("%d is a leap year", n);
     }
-    else
-    {
+    else if(n % 4 == 0 && n % 100 != 0){
+        printf("%d is a leap year", n);
+    }
+    else{
         printf("%d is not a leap year", n);
-    } 
+    }
     return 0;
 }
