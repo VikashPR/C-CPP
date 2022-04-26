78c4f6bf7cafbae6db0b41026cad9e50a7d715d8
VikashPR <vikashrajendran2806@gmail.com>
2022-04-26 09:22:12 +0530

    Feat: find leap year add

--- CCC/basic.c
+++ CCC/basic.c
@@ -1,6 +1,31 @@
-#include<stdio.h>
-int main(){
-    int x = 5;
-    int y = x++ + ++x + ++x;
-    printf("%d\n", y);
+#include <stdio.h>
+
+int main()
+{  
+    int n;
+    scanf("%d", &n);
+    // find if the given  year is a leap year or not
+    if (n % 4 == 0)
+    {
+        if (n % 100 == 0)
+        {
+            if (n % 400 == 0)
+            {
+                printf("%d is a leap year", n);
+            }
+            else
+            {
+                printf("%d is not a leap year", n);
+            }
+        }
+        else
+        {
+            printf("%d is a leap year", n);
+        }
+    }
+    else
+    {
+        printf("%d is not a leap year", n);
+    } 
+    return 0;
 }
