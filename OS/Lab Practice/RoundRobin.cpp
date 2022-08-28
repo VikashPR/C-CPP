37255c3a1284b19d9352105193b23773eccf9706
VikashPR <vikashrajendran2806@gmail.com>
2022-08-28 06:52:30 +0530

    refactor: code indend

--- OS/Lab Practice/RoundRobin.cpp
+++ OS/Lab Practice/RoundRobin.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 using namespace std;
 
+
 void FindWaitingTime(int process[], int n, int bt[],int wt, int quantum){
     
 }
--- OS/Lab/fcfs.cpp
+++ OS/Lab/fcfs.cpp
@@ -1,23 +1,23 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 void findWaitingTime(int processes[], int n,
-						int bt[], int wt[])
+					 int bt[], int wt[])
 {
 	wt[0] = 0;
 
-	for (int i = 1; i < n ; i++ )
-		wt[i] = bt[i-1] + wt[i-1] ;
+	for (int i = 1; i < n; i++)
+		wt[i] = bt[i - 1] + wt[i - 1];
 }
 
-void findTurnAroundTime( int processes[], int n,
-				int bt[], int wt[], int tat[])
+void findTurnAroundTime(int processes[], int n,
+						int bt[], int wt[], int tat[])
 {
-	for (int i = 0; i < n ; i++)
+	for (int i = 0; i < n; i++)
 		tat[i] = bt[i] + wt[i];
 }
 
-void findavgTime( int processes[], int n, int bt[])
+void findavgTime(int processes[], int n, int bt[])
 {
 	int wt[n], tat[n], total_wt = 0, total_tat = 0;
 
@@ -25,26 +25,28 @@ void findavgTime( int processes[], int n, int bt[])
 
 	findTurnAroundTime(processes, n, bt, wt, tat);
 
-	cout << "Processes "<< " Burst time "
-		<< " Waiting time " << " Turn around time\n";
+	cout << "Processes "
+		 << " Burst time "
+		 << " Waiting time "
+		 << " Turn around time\n";
 
-	for (int i=0; i<n; i++)
+	for (int i = 0; i < n; i++)
 	{
 		total_wt = total_wt + wt[i];
 		total_tat = total_tat + tat[i];
-		cout << " " << i+1 << "\t\t" << bt[i] <<"\t "
-			<< wt[i] <<"\t\t " << tat[i] <<endl;
+		cout << " " << i + 1 << "\t\t" << bt[i] << "\t "
+			 << wt[i] << "\t\t " << tat[i] << endl;
 	}
 
 	cout << "Average waiting time = "
-		<< (float)total_wt / (float)n;
+		 << (float)total_wt / (float)n;
 	cout << "\nAverage turn around time = "
-		<< (float)total_tat / (float)n;
+		 << (float)total_tat / (float)n;
 }
 
 int main()
 {
-	int processes[] = { 1, 2, 3};
+	int processes[] = {1, 2, 3};
 	int n = sizeof processes / sizeof processes[0];
 
 	int burst_time[] = {10, 5, 8};
@@ -52,4 +54,3 @@ int main()
 	findavgTime(processes, n, burst_time);
 	return 0;
 }
-
