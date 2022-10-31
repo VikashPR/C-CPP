0abbbebeb0fe0d3b22edfb050927c1fa1b6d7de7
VikashPR <vikashrajendran2806@gmail.com>
2022-10-31 10:34:20 +0530

    feat: non recursive Inorder traversal add

--- CCC/main.c
+++ CCC/main.c
@@ -1,31 +1,26 @@
 #include <stdio.h>
-void quickSort(int arr[], int low, int high)
-{
-    if (low < high)
-    {
-        int pi = partition(arr, low, high);
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
-}
-
-void partition(int arr[], int low, int high)
-{
-    int pivot = arr[high];
-    int i = (low - 1);
-    for (int j = low; j <= high - 1; j++)
-    {
-        if (arr[j] < pivot)
-        {
-            i++;
-            swap(&arr[i], &arr[j]);
-        }
-    }
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
+void Inorder(struct BinaryTreeNode *root){
+    struct Node *current = root;
+    sturct Node *S = NULL;
+    bool done = 0;
 }
 
 int main()
 {
+    while(!done){
+        if(current != NULL){
+            Push(&S, current);
+            current = current->left;
+        }
+        else{
+            if(!isEmpty(S)){
+                current = Pop(&S);
+                printf("%d ", current->data);
+                current = current->right;
+            }
+            else
+                done = 1;
+        }
+    }
     return 0;
 }
